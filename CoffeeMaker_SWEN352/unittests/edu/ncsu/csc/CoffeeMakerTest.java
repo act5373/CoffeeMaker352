cec0ad6f4d1de7b68b42faf8b1b1a90c77272d0f
Cornchipss <cornchipgonecodin@gmail.com>
2021-09-07 17:40:26 -0400

    Initial Commit

---
+++ CoffeeMaker_SWEN352/unittests/edu/ncsu/csc/CoffeeMakerTest.java
@@ -0,0 +1,49 @@
+package edu.ncsu.csc;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import coffeemaker.CoffeeMaker;
+import coffeemaker.exceptions.InventoryException;
+import junit.framework.TestCase;
+
+public class CoffeeMakerTest extends TestCase
+{	
+	@Before
+	public void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception 
+	{
+		super.tearDown();
+	}
+
+	@Test
+	public void test()
+	{
+		CoffeeMaker cm = new CoffeeMaker();
+		
+		assertThrows(InventoryException.class, () ->
+		{
+			cm.addInventory("one", "0", "0", "0");
+		});
+		
+		assertThrows(InventoryException.class, () ->
+		{
+			cm.addInventory("-10", "0", "0", "0");
+		});
+		
+		assertThrows(InventoryException.class, () ->
+		{
+			cm.addInventory("10000000000000000", "0", "0", "0");
+		});
+		
+		
+	}
+}
---
+++ CoffeeMaker_SWEN352/unittests/edu/ncsu/csc/RecipeTest.java
@@ -0,0 +1,30 @@
+package edu.ncsu.csc;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+class RecipeTest extends TestCase
+{	
+	@Before
+	public void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception 
+	{
+		super.tearDown();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
