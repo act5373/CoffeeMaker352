826a6e4637906ea17efad5ed1a04133ee9abac11
jks6634 <jks6634@rit.edu>
2021-09-09 17:16:00 -0400

    Update RecipeTest.java

--- CoffeeMaker_SWEN352/unittests/edu/ncsu/csc/RecipeTest.java
+++ CoffeeMaker_SWEN352/unittests/edu/ncsu/csc/RecipeTest.java
@@ -1,71 +1,232 @@
 package edu.ncsu.csc;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import coffeemaker.Recipe;
+import coffeemaker.exceptions.*;
+
+import static org.junit.Assert.fail;
 
 import org.junit.After;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
-import coffeemaker.Recipe;
-import coffeemaker.exceptions.RecipeException;
 import junit.framework.TestCase;
 
+
+//Written by Joseph Saltalmacchia
+//Test class for Recipe class in coffeemaker
+//note: we use "BerforeEach" and "AfterEach" instead of "Before" and "After" for the setup and teardown functions because we are using Junit 5
 class RecipeTest extends TestCase
 {	
-	Recipe rec;
+	private Recipe recipe;
 	
 	@BeforeEach
 	public void setUp() throws Exception
 	{
+		this.recipe = new Recipe();
 		super.setUp();
-		
-		rec = new Recipe();
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception 
 	{
-		super.tearDown();		
+		recipe = null;
+		super.tearDown();
 	}
 	
 	@Test
-	public void setAmtChocolateTest()
-	{
-		assertThrows(RecipeException.class, () ->
-		{
-			rec.setAmtChocolate("-1");
-		});
-		
-		assertThrows(RecipeException.class, () ->
-		{
-			rec.setAmtChocolate("a");
-		});
-		
-		assertThrows(RecipeException.class, () ->
-		{
-			rec.setAmtChocolate("-1");
-		});
-		
-		assertThrows(RecipeException.class, () ->
-		{
-			rec.setAmtChocolate("100000000000000000");
-		});
-		
-		try
-		{
-			assertEquals(0, rec.getAmtChocolate());			
-			rec.setAmtChocolate("0");
-			assertEquals(0, rec.getAmtChocolate());
-			
-			rec.setAmtChocolate("1");
-			assertEquals(1, rec.getAmtChocolate());
-			
-			rec.setAmtChocolate("10000000");
-			assertEquals(10000000, rec.getAmtChocolate());
+	public void testGetName() {
+		assertEquals("", recipe.getName()); // arguments: error message, arg1, arg2
+	}
+	
+	@Test
+	public void testGetPrice() {
+		assertEquals(0, recipe.getPrice());
+	}
+	
+	//**************************
+	// Amount Coffee test cases
+	//**************************
+	
+	@Test
+	public void testGetAmntCoffee() {
+		assertEquals(0, recipe.getAmtCoffee());
+	}
+	
+	@Test
+	public void testSetAmntCoffeeValid1(){
+		try{
+			recipe.setAmtCoffee("1");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
 		}
-		catch(RecipeException ex)
-		{
-			throw new RuntimeException(ex);
+		
+		assertEquals(1, recipe.getAmtCoffee());
+	}
+	
+	@Test
+	public void testSetAmntCoffeeValid0(){
+		try{
+			recipe.setAmtCoffee("0");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(0, recipe.getAmtCoffee());
+	}
+	
+	@Test
+	public void testSetAmntCoffeeValidNominal(){
+		try{
+			recipe.setAmtCoffee("15");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(15, recipe.getAmtCoffee());
+	}
+	
+	@Test
+	public void testSetAmntCoffeeInvalidABC(){
+		try{
+			recipe.setAmtCoffee("ABC");
+		} catch(RecipeException e) {
+			assertEquals(e.getMessage(), "Units of coffee must be a positive integer");
+		}
+	}
+	
+	@Test
+	public void testSetAmntCoffeeInvalidNeg1(){
+		try{
+			recipe.setAmtCoffee("-1");
+		} catch(RecipeException e) {
+			assertEquals(e.getMessage(), "Units of coffee must be a positive integer");
+		}
+	}
+	
+	@Test
+	public void testGetAmntMilk() {
+		assertEquals(0, recipe.getAmtMilk());
+	}
+	
+	//*************************
+	//Amount sugar Test cases
+	//*************************
+	
+	@Test
+	public void testGetAmntSugar() {
+		assertEquals(0, recipe.getAmtSugar());
+	}
+	
+	@Test
+	public void testSetAmntSugarValid1(){
+		try{
+			recipe.setAmtSugar("1");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(1, recipe.getAmtSugar());
+	}
+	
+	@Test
+	public void testSetAmntSugarValid0(){
+		try{
+			recipe.setAmtSugar("0");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(0, recipe.getAmtSugar());
+	}
+	
+	@Test
+	public void testSetAmntSugarValidNominal(){
+		try{
+			recipe.setAmtSugar("15");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(15, recipe.getAmtSugar());
+	}
+	
+	@Test
+	public void testSetAmntSugarInvalidABC(){
+		try{
+			recipe.setAmtSugar("ABC");
+		} catch(RecipeException e) {
+			assertEquals(e.getMessage(), "Units of sugar must be a positive integer");
+		}
+	}
+	
+	@Test
+	public void testSetAmntSugarInvalidNeg1(){
+		try{
+			recipe.setAmtSugar("-1");
+		} catch(RecipeException e) {
+			assertEquals(e.getMessage(), "Units of sugar must be a positive integer");
+		}
+	}
+	
+	
+	//*****************************
+	// Amount Chocolate Test cases
+	//*****************************
+	
+	@Test
+	public void testGetAmntChocolate() {
+		assertEquals( 0, recipe.getAmtChocolate());
+	}
+	
+	@Test
+	public void testSetAmntChocolateValid1(){
+		try{
+			recipe.setAmtChocolate("1");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(1, recipe.getAmtChocolate());
+	}
+	
+	@Test
+	public void testSetAmntChocolateValid0(){
+		try{
+			recipe.setAmtChocolate("0");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(0, recipe.getAmtChocolate());
+	}
+	
+	@Test
+	public void testSetAmntChocolateValidNominal(){
+		try{
+			recipe.setAmtChocolate("15");
+		} catch(RecipeException e) {
+			fail("Should parse Int");
+		}
+		
+		assertEquals(15, recipe.getAmtChocolate());
+	}
+	
+	@Test
+	public void testSetAmntChocolateInvalidABC(){
+		try{
+			recipe.setAmtChocolate("ABC");
+		} catch(RecipeException e) {
+			assertEquals(e.getMessage(), "Units of chocolate must be a positive integer");
+		}
+	}
+	
+	@Test
+	public void testSetAmntChocolateInvalidNeg1(){
+		try{
+			recipe.setAmtChocolate("-1");
+		} catch(RecipeException e) {
+			assertEquals(e.getMessage(), "Units of chocolate must be a positive integer");
 		}
 	}
 }
