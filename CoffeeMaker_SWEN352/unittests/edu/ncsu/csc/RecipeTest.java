b1bd47318968ced66fd1775bc68db1fc2ea430a9
Cornchipss <cornchipgonecodin@gmail.com>
2021-09-09 16:34:11 -0400

    Wrote some tests for Recipe's setAmtChocolate method

--- CoffeeMaker_SWEN352/unittests/edu/ncsu/csc/RecipeTest.java
+++ CoffeeMaker_SWEN352/unittests/edu/ncsu/csc/RecipeTest.java
@@ -1,30 +1,71 @@
 package edu.ncsu.csc;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import coffeemaker.Recipe;
+import coffeemaker.exceptions.RecipeException;
 import junit.framework.TestCase;
 
 class RecipeTest extends TestCase
 {	
-	@Before
+	Recipe rec;
+	
+	@BeforeEach
 	public void setUp() throws Exception
 	{
 		super.setUp();
+		
+		rec = new Recipe();
 	}
 
 	@After
 	public void tearDown() throws Exception 
 	{
-		super.tearDown();
+		super.tearDown();		
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setAmtChocolateTest()
+	{
+		assertThrows(RecipeException.class, () ->
+		{
+			rec.setAmtChocolate("-1");
+		});
+		
+		assertThrows(RecipeException.class, () ->
+		{
+			rec.setAmtChocolate("a");
+		});
+		
+		assertThrows(RecipeException.class, () ->
+		{
+			rec.setAmtChocolate("-1");
+		});
+		
+		assertThrows(RecipeException.class, () ->
+		{
+			rec.setAmtChocolate("100000000000000000");
+		});
+		
+		try
+		{
+			assertEquals(0, rec.getAmtChocolate());			
+			rec.setAmtChocolate("0");
+			assertEquals(0, rec.getAmtChocolate());
+			
+			rec.setAmtChocolate("1");
+			assertEquals(1, rec.getAmtChocolate());
+			
+			rec.setAmtChocolate("10000000");
+			assertEquals(10000000, rec.getAmtChocolate());
+		}
+		catch(RecipeException ex)
+		{
+			throw new RuntimeException(ex);
+		}
 	}
-
 }
